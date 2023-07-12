3ff47de149f04a68222fdf7793f100caccd4d42c
Manel-Os <oulds.manel@gmail.com>
2023-07-12 15:08:00 -0400

    Phase 2 completed

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+import { Provider } from "react-redux";
+import { populateProduce } from "./store/produce";
 
 function Root() {
+  const store = configureStore();
+  if (process.env.NODE_ENV !== "production") {
+    window.store = store;
+    window.populateProduce = populateProduce;
+  }
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
@@ -16,5 +26,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
